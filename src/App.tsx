cfbce8871cd84325ac82be4fd144f8b09624a6f7
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-04-28 19:35:27 +0900

    modified:   src/App.tsx 	new file:   src/pages/LatestHomePage.tsx 	deleted:    src/pages/Test.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 // Import the Test component
-import Test from './pages/Test';
+import LatestHomePage from './pages/LatestHomePage';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,13 +9,8 @@ function App() {
   return (
     <>
       {/* Include the Test component here */}
-      <Test />
-      <div>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>
-          Increment
-        </button>
-      </div>
+      <LatestHomePage/>
+ 
     </>
   );
 }
---
+++ src/pages/LatestHomePage.tsx
@@ -0,0 +1,18 @@
+
+export default function LatestHomePage(){
+    return( <div>
+        <div className = "latest-article">
+            <h2>
+                업데이트 된 글 
+            </h2>
+
+        </div>
+            
+        
+        <body>
+            
+           title- 최신글제목 - title 최신글제목반복 
+        </body>
+    </div>
+    )
+}

f03a0bc2041486614ca3ee378cd205f6d74a1050
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-04-28 16:44:06 +0900

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import react from 'Reeact'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+// Import the Test component
+import Test from './pages/Test';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      {/* Include the Test component here */}
+      <Test />
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>
+          Increment
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

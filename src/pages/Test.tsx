63f4462c3380ae7fb67e5edfc8d3c10dd0450077
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-04-28 16:40:19 +0900

    modified:   src/App.tsx 	new file:   src/pages/Test.tsx

---
+++ src/pages/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Test(){
+    return <div>
+        <title>
+            Test 제목 입니다
+        </title>
+        <body>
+            Test Body 입니다.
+        </body>
+    </div>
+}

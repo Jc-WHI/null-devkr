674b1a2421d5a3dc19f9f7ffbe65b0641b654d19
NullemEPaR <81462719+Jc-WHI@users.noreply.github.com>
2025-05-07 11:53:36 +0900

    test

--- src/pages/LatestHomePage.tsx
+++ src/pages/LatestHomePage.tsx
@@ -1,18 +1,42 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function LatestHomePage(){
-    return( <>
-        <div className = "latest-article">
-            <h2>
-                Jo's Diary
-            </h2>
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        </div>
-            
-        
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
         <body>
-            
-           이 블로그는 현재 next.js로 개발중입니다. <br/> 
+          <Main />
+          <NextScript />
         </body>
-    </>
-    )
+      </Html>
+    );
+  }
 }
